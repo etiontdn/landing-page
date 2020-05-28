1189549381bfa9dca9dfc89f4fd66ae2e49d7104
Etiontdn <etiontdn@gmail.com>
2020-05-28 16:04:00 -0300

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="has-background-backgroundcolor">
+    <Navbar></Navbar>
+    <transition name="page" class="test">
+      <PageContent v-if="isLoaded" id="pagecontent"></PageContent>
+    </transition>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PageContent from './components/PageContent.vue';
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
-  name: "App",
+  name: 'App',
+  data: function () {
+    return {
+      isLoaded: false
+    }
+  },
   components: {
-    HelloWorld
-  }
+    PageContent,
+    Navbar,
+    Footer,
+  },
+  mounted: function (){
+    this.isLoaded = true;
+  },
 };
+
+
 </script>
 
 <style lang="scss">
+@import 'main.scss';
+.page-enter-active, .page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Rubik', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  .title {
+    font-family: 'Rubik Mono One', sans-serif;
+  }
 }
+
+.button {
+  &:active {
+    transform: scale(0.96);
+  }
+}
+
+body {
+  min-width: 320px;
+}
+@import '~bulma';
+@import '~buefy/src/scss/buefy';
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero has-background-primary is-small">
+    <div class="hero-body">
+      <div class="container">
+        <div class="logo is-flex">
+          <figure class="image is-64x64 is-hidden-mobile">
+            <img src="@/assets/potato.svg" alt="placeholder+image" />
+          </figure>
+          <div class="">
+            <h1 class="title">
+              Batata Home
+            </h1>
+
+            <h2 class="subtitle is-hidden-mobile">
+              A solução para todos os seus problemas.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+<style lang="scss" scoped>
+.logo {
+  align-items: stretch;
+  .image {
+    @media only screen and (min-width: 600px) {
+      margin-right: 15px;
+    }
+  }
+}
+</style>
